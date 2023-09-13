8ccdd91d5ceca318e944ea25c3806f3282b906ce
Koby <kobysac@gmail.com>
2023-09-13 00:01:14 -0700

    added api URIs

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const API_KEY = "e68d5f8f7c296f931478d8dddf6838c0";
+
+const currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather";
+const forecastWeatherAPI = "https://api.openweathermap.org/data/2.5/forecast?";
+const geoCoderAPI = "https://api.openweathermap.org/geo/1.0/direct";
