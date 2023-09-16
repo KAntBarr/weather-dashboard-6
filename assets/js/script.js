66bcb3e8ba16165d2533af1162be11a9d3767ad7
Koby <kobysac@gmail.com>
2023-09-16 00:13:12 -0700

    messed around with bootstrap, no changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ const API_KEY = "e68d5f8f7c296f931478d8dddf6838c0";
 const currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather";
 const forecastWeatherAPI = "https://api.openweathermap.org/data/2.5/forecast?";
 const geoCoderAPI = "https://api.openweathermap.org/geo/1.0/direct";
+
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+}
